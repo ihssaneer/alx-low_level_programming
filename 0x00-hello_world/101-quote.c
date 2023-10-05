d504baad7713cb05eb8763340c5c93661883ba84
ihssaneer <errajy.ihssane@gmail.com>
2023-10-06 00:41:09 +0100

    Creating 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - Entry point.
+* Description: This program prints a specifique messege.
+* Return: retutns 1.
+*/
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+return (1);
+}
