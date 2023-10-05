242ae02c274cb8a2b8103e3e3361f7f573b619b1
ihssaneer <errajy.ihssane@gmail.com>
2023-10-06 00:11:11 +0100

    Creating 4-puts.c v2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
-* main : Entry point.
-* 
-* Return: retutns 0. 
+* main - Entry point.
+* Description: This program prints a messege using puts function.
+* Return: retutns 0.
 */
-int main ()
+int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
