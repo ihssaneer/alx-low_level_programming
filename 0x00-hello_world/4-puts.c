6a271e34f28c5bbcdb68a39d99ea94cda3bbb2e4
ihssaneer <errajy.ihssane@gmail.com>
2023-10-05 23:22:17 +0100

    Creating 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main : Entry point.
+* 
+* Return: retutns 0. 
+*/
+int main ()
+{
+    puts(""Programming is like building a multilingual puzzle");
+    return (0);
+}
