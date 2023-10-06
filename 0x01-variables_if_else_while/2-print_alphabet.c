103f9b20d5b29be5066b58dd35bfc4fa11df869c
ihssaneer <errajy.ihssane@gmail.com>
2023-10-06 23:29:22 +0100

    Creating 2-print_alphabet.c.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - This program prints the alphabet in lowercase,
+* followed by a new line, using only putchar twice.
+* Return: Always 0.
+*/
+int main(void)
+{
+	char charc,
+
+	for (charc = 'a'; charc <= 'z'; charc++)
+	{
+		putchar ('charc');
+	}
+	putchar ('\n');
+
+	return (0);
+}
