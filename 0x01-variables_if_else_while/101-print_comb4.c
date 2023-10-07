99dabb3dcaa4b4e878effa0f0d4ef5c210f531d1
ihssaneer <errajy.ihssane@gmail.com>
2023-10-07 01:08:46 +0100

    Creating 101-print_comb4.c.

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+* main - This program prints all possible combinations
+* of two two-digit numbers.
+* Return: Always 0.
+*/
+int main(void)
+{
+	int d1, d2, d3;
+
+	for (d1 = 0; d1 <= 7; d1++)
+	{
+		for (d2 = d1 + 1; d2 <= 8 ; d2++)
+		{
+			for (d3 = d2 + 1; d3 <= 9 ; d3++)
+			{
+				putchar (d1 + '0');
+				putchar (d2 + '0');
+				putchar (d3 + '0');
+				if (d1 != 7)
+				{
+					putchar (',');
+					putchar (' ');
+				}
+			}
+		}
+	}
+	putchar ('\n');
+
+	return (0);
+}
