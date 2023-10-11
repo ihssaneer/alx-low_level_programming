f797318a7869d73ae287c405bba01725ff491f84
ihssaneer <errajy.ihssane@gmail.com>
2023-10-11 01:25:17 +0100

    Creating 1-alphabet.c V1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * main - prints the alphabet, in lowercase, followed by a new line.
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line.
  * Return: 0 success.
  */
-int main(void)
+void print_alphabet(void)
 {
 	int count;
 
@@ -13,5 +13,4 @@ int main(void)
 		_putchar(count);
 	}
 		_putchar('\n');
-	return (0);
 }
