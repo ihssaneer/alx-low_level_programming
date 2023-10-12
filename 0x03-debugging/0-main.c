cbecda9dfd1503424780dab81d547f19d4cf78a3
ihssaneer <errajy.ihssane@gmail.com>
2023-10-12 23:34:29 +0100

    Creating 0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**ihssaneer**/
+/**
+ * main - test that the function positive_or_negative()
+ * gives the correct output when given a case of 0.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x = 0;
+
+	positive_or_negative(x);
+
+	return (0);
+}
+
