52d711d49eb4d25726eacb28cce8af2ce58a45de
ihssaneer <errajy.ihssane@gmail.com>
2023-10-13 23:34:23 +0100

    creating 101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/*ihssaneer*/
+/**
+ * print_number - prints an integer.
+ * @n: the integer to print.
+ * Return: void.
+ */
+void print_number(int n)
+{
+	unsigned int i = 1, j, count = 0;
+	unsigned int N = n, P;
+
+	if (n < 0)
+		N = -N, _putchar(45);
+	P = N;
+	while (N >= 10)
+	{
+		N = N / 10, i *= 10, count++;
+	}
+	for (j = 0; j <= count; j++)
+	{
+		N = P / i, i /= 10, _putchar((N % 10) + '0');
+	}
+}
