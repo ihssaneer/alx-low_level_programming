67c8e3653800db6bc17c9f34f445a584d70fcd8f
ihssaneer <errajy.ihssane@gmail.com>
2023-10-23 23:32:18 +0100

    Creating 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strspn - Gets the length of a prefix substring
+ * @s: String to be checked
+ * @accept: Bytes that will determine the length
+ * Return: Length of the bytes given
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	int i;
+
+	while (s[count] != '\0')
+	{
+		i = 0;
+		while (accept[i] != s[count])
+		{
+			if (accept[i] == '\0')
+			{
+			return (count);
+			}
+			j++;
+		}
+
+	count++;
+	}
+	return (count);
+}
