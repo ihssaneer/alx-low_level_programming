0201a20c972b5fe527359753708e06117fbd2e76
ihssaneer <errajy.ihssane@gmail.com>
2023-10-23 22:10:18 +0100

    Creating 2-strchr.c v4

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stddef.h>
 /**
  * _strchr - locates a character in a string.
  * @s: String to be checked.
@@ -20,5 +20,5 @@ char *_strchr(char *s, char c)
 		}
 		i++;
 	}
-	return (NULL);
+return (NULL);
 }
