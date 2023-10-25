ab5c3cb34e7588b8e349ff4cf7eea49fac1c0bf6
ihssaneer <errajy.ihssane@gmail.com>
2023-10-25 21:36:01 +0100

    CR 101-wildcmp.c v1

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -3,29 +3,20 @@
 /**
  * wildcmp - compares two strings.
  *@s1: the string to compare.
- * @s2: the string to compare.
+ *@s2: the string to compare.
  * Return:  returns 1 success, otherwise return 0.
  */
 int wildcmp(char *s1, char *s2)
 {
-	int i = 1;
-
-	if (n == 0 || n == 1)
-		return (n);
-	else if (n < 0)
-		return (-1);
-	else if (n >= 2)
+	if (*s1 == *s2 && *s1)
 	{
-		if (n == i * i && n % i == 0)
-		{
-			return (i);
-		}
-		else
-		{
-			i++;
-			_sqrt_recursion(n);
-		}
+		s1++;
+		s2--;
+		return (wildcmp(s1, s2));
 	}
-return (-1);
+	if (*s1 != *s2)
+	{
+		return (0);
+	}
+	return (1);
 }
-
