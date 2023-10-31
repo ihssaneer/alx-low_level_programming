10490c7c177b8112a39d77806cb3a3c2e4de6192
ihssaneer <errajy.ihssane@gmail.com>
2023-10-31 11:26:08 +0100

    Creating main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <string.h>
+
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
