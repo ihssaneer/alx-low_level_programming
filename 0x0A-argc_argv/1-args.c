2623975d27b9479e995c887230a599092dfc84df
ihssaneer <errajy.ihssane@gmail.com>
2023-11-01 09:12:01 +0100

    Creating 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* main - prints the number of arguments passed into it, followed by a new line.
+* @argc: The number of command-line arguments
+* @argv: An array of strings representing the arguments
+* Return: Always 0.
+*/
+int main(int argc, char *argv[])
+{
+(void) argv;
+printf("%d\n", argc);
+return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,7 @@
+#ifndef ARGV_ARGC_H
+#define ARGV_ARGC_H
+
+#include <stdio.h>
+int _putchar(char c);
+
+#endif /*ARGV_ARGC_H*/
