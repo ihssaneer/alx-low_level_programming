32583bad588c3157aa49ddccf8fef2a883da01c4
ihssaneer <errajy.ihssane@gmail.com>
2023-11-07 00:09:01 +0100

    CR 2-print_dog.c v1

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,7 +7,13 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	printf("Name: %s\n", (*d).name);
+	if ((*d).name != NULL)
+		printf("Name: %s\n", (*d).name);
+	else
+		printf("Name: (nil)");
 	printf("Age: %.6f\n", (*d).age);
-	printf("Owner: %s\n", (*d).owner);
+	if ((*d).owner != NULL)
+		printf("Owner: %s\n", (*d).owner);
+	else
+		printf("Owner: (nil)");
 }
