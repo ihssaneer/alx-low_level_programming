41425739362d72473762dd6e9f9bfda29cc39445
ihssaneer <errajy.ihssane@gmail.com>
2023-11-06 17:25:21 -0800

    CR files

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,7 @@
+#ifndef TASK0_H
+#define TASK0_H
+
+#define SIZE 1024
+
+#endif /*TASK_H*/
+
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,7 @@
+#ifndef TASK4_H
+#define TASK4_H
+
+#define SUM(x,y) ((x) + (y))
+
+#endif /*TASK4_H*/
+
