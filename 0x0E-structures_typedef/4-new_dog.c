f9469f74c23645598d2189bbcdb081328b9f88a6
ihssaneer <errajy.ihssane@gmail.com>
2023-11-06 16:28:31 -0800

    CR file 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -4,10 +4,12 @@
  * @name: the dog name.
  * @age: the dog age.
  * @owner: the dog owner.
+ * Return: pointer to the newly allocated memory.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog_ptr = NULL;
+
 	dog_ptr = (dog_t *)malloc(sizeof(dog_t));
 	if (dog_ptr == NULL)
 		return (NULL);
