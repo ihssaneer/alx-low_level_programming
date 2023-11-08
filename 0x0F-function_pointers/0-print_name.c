4034e1519aae43fb37e40db9dccb1aa9b6bcf367
ihssaneer <errajy.ihssane@gmail.com>
2023-11-08 15:52:17 +0100

    CR file 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+ * print_name -  prints a name.
+ * @name: the name to print.
+ * @f: a pointer to a function that take a string as an arguments.
+ * Return: void.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+    if (name == NULL)
+        return;
+    (*f)(name);
+}
