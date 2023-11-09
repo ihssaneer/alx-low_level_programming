698dc2f6ca922b07fe39eb4761db0e5f9eb57cb7
ihssaneer <errajy.ihssane@gmail.com>
2023-11-09 20:55:00 +0100

    CR file 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,20 +1,20 @@
 #include "3-calc.h"
 /**
- * get_op_func - 
- * @s:
- * Return:
+ * get_op_func - call the correct function.
+ * @s: the operator used.
+ * Return: the result of the operation.
  */
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
 
 	for (i = 0; ops[i].op != NULL; i++)
 	{
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,28 +7,29 @@
  */
 int main(int ac, char **av)
 {
-    int result = 0, num1, num2;
-    char OP;
+	int result = 0, num1, num2;
+	char OP;
 
-    if (ac != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
-    num1 = atoi(av[1]);
-    num2 = atoi(av[3]);
-    OP = av[2][0];
-    if (av[2][1] != '\0' || (OP != '/' && OP != '%' && OP != '+' && OP != '-' && OP != '*'))
-    {
-        printf("Error\n");
-        return (99);
-    }
-    if (num2 == 0 || (OP == '/' && OP == '%'))
-    {
-        printf("Error\n");
-        exit(100);
-    }
-    result = (get_op_func(av[2]))(num1, num2);
-    printf("%d\n", result);
-    return (0);
+	if (ac != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(av[1]);
+	num2 = atoi(av[3]);
+	OP = av[2][0];
+	if (av[2][1] != '\0' || (OP != '/' && OP != '%'
+				&& OP != '+' && OP != '-' && OP != '*'))
+	{
+		printf("Error\n");
+		return (99);
+	}
+	if (num2 == 0 || (OP == '/' && OP == '%'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	result = (get_op_func(av[2]))(num1, num2);
+	printf("%d\n", result);
+	return (0);
 }
