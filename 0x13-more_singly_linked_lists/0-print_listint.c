d6b67ee0624f1bc645aca9038571863f0e2e96f4
ihssaneer <errajy.ihssane@gmail.com>
2023-11-20 19:13:08 +0100

    Creating files V4

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,5 @@
 #include "lists.h"
 
-int _putchar(char c);
-size_t print_listint(const listint_t *h);
 /**
  * print_listint - prints all the elements of a listint_t list,
  * followed by new line.
@@ -14,19 +12,9 @@ size_t print_listint(const listint_t *h)
 
 	while (h != NULL)
 	{
-		printf("%i\n", h->n);
+		printf("%d\n", h->n);
 		count++;
 		h = h->next;
 	}
 	return (count);
-}
-
-/**
- * _putchar - write a character to the stdout.
- * @c: charachter to print.
- * Return: 1 on sucess.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
 }
