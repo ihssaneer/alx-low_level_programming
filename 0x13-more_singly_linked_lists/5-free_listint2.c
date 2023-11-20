1b89acebfbe060bf452aba41c69107678837a573
ihssaneer <errajy.ihssane@gmail.com>
2023-11-20 19:24:54 +0100

    Creating fil 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * free_listint -  frees a listint_t list.
+ * free_listint2 -  frees a listint_t list.
  * @head: a pointer to a listint_t list.
  * Return: void.
  */
-void free_listint(listint_t *head)
+void free_listint2(listint_t **head)
 {
 	listint_t *tmp = NULL;
 
