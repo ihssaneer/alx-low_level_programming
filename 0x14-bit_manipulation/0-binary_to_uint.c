ee968105cd93a6d9c8eb58eb58be6320a7c0d425
ihssaneer <errajy.ihssane@gmail.com>
2023-11-24 00:12:04 +0100

    CR files v1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,21 +8,19 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i = 0;
-	unsigned int dec_n = 0;
+	unsigned int nm = 0, i = 0;
 
-	if (b == NULL)
-		return (0);
-	if (strlen(b) == 8)
+	while (b[i] != '\0')
 	{
-	for (; b[i] != '\0'; i++)
-	{
-		if (b[i] == '0' || b[i] == '1')
+		if (b[i] == '1' || b[i] == '0')
 		{
-			dec_n += 2
+			nm = nm << 1;
+			if (b[i] & 1)
+				nm += 1;
 		}
 		else
 			return (0);
+		i++;
 	}
-	}
+	return (nm);
 }
