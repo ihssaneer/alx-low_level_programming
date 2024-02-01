5ab1624e6f92b0c8ce10c182b1d09dc1d673da35
ihssaneer <errajy.ihssane@gmail.com>
2024-02-01 21:57:28 +0100

    CR f 2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -7,5 +7,10 @@
 */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
+	unsigned long int index = 0;
 
+	if (key ==  NULL || size == 0)
+		return (-1);
+	index = hash_djb2(key) % size;
+	return (index);
 }
