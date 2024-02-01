b60f4edbcd3bafa705208c8a2abdcb2324006369
ihssaneer <errajy.ihssane@gmail.com>
2024-02-01 18:49:03 +0100

    Creating f 0 and Readmed

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - hhd
+ * @size: sdjhj
+ * Return: jhdj
+*/
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_tab = NULL;
+
+	if (size <= 0)
+		return (NULL);
+	hash_tab = malloc(sizeof(hash_table_t *) * size);
+	if (hash_tab == NULL)
+		return (NULL);
+	return (hash_tab);
+}
